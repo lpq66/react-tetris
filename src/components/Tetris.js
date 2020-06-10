914239718cf911a25a7c408f1e694dc0652f7695
Walter <vldlbrk@gmail.com>
2020-06-09 23:42:55 -0500

    Add minimal scene styling

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -4,20 +4,24 @@ import Display from './Display';
 import StartButton from './StartButton';
 
 import { createStage } from '../gameHelpers';
+import { StyledTetris } from './styles/StyledTetris';
+import { StyledTetrisWrapper } from './styles/StyledTetrisWrapper';
 
 const Tetris = ({ type }) => {
 	return (
-		<div>
-			<Stage stage={createStage()} />
-			<aside>
-				<div>
-					<Display text="Score" />
-					<Display text="Rows" />
-					<Display text="Level" />
-				</div>
-				<StartButton />
-			</aside>
-		</div>
+		<StyledTetrisWrapper>
+			<StyledTetris>
+				<Stage stage={createStage()} />
+				<aside>
+					<div>
+						<Display text="Score" />
+						<Display text="Rows" />
+						<Display text="Level" />
+					</div>
+					<StartButton />
+				</aside>
+			</StyledTetris>
+		</StyledTetrisWrapper>
 	)
 }
 
---
+++ src/components/styles/StyledButton.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.div`
+	box-sizing: border-box;
+	padding: 20px;
+	margin-bottom: 20px;
+	color: white;
+	border-radius: 20px;
+	border: none;
+	background: #333;
+	font-family: Pixel, Arial;
+	color: white;
+	font-size: 1rem;
+	outline: none;
+	cursor: pointer;
+`
