21fcaacc0763b1e79fbf292abc01fc557bb55504
Walter <vldlbrk@gmail.com>
2020-06-15 23:21:20 -0500

    fixed Math.random error

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -8,7 +8,7 @@ export const usePlayer = () => {
 
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    //tetromino: randomTetromino().shape,
+    tetromino: randomTetromino().shape,
     collided: false,
   })
 
--- src/tetrominos.js
+++ src/tetrominos.js
@@ -64,7 +64,7 @@ export const TETROMINOS = {
 
 export const randomTetromino = () => {
 	const tetrominos = 'IJLOSTZ';
-	const randTetromino = tetrominos[Math.floor(Math.Random() * tetrominos.length)];
+	const randTetromino = tetrominos[Math.floor(Math.random() * tetrominos.length)];
 
 	return TETROMINOS[randTetromino];
 }
